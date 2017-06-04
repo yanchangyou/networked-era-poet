e03b7f677e751248de4c39caa37285140e656a5c
yanchangyou <yanchangyou@gmail.com>
2017-06-04 09:05:05 +0800

    代码重构，首页分离

--- pages/poem/create/index.js
+++ pages/poem/create/index.js
@@ -1,10 +1,11 @@
-//index.js
 
-var md5 = require('../../utils/md5.js')
+var md5 = require('../../../utils/md5.js')
 
-//获取应用实例
 var app = getApp()
 Page({
+  /**
+   * 数据
+   */
   data: {
     poems: [["大江桥上得南花",
       "漠北端容此意加",
@@ -16,13 +17,77 @@ Page({
     progressDisplay: "none",
     progressPercent: 0,
     poemContentDisplay: "",
-    showNextPomeDisplay:"none"
+    showNextPomeDisplay: "none"
   },
-  setPoemKeywords: function(e) {
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function () {
+    console.log('onLoad')
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function (userInfo) {
+      //更新数据
+      that.setData({
+        userInfo: userInfo
+      })
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  setPoemKeywords: function (e) {
     var poemKeywords = e.detail.value;
-    this.setData({ poemKeywords: poemKeywords});
+    this.setData({ poemKeywords: poemKeywords });
   },
-  makePoem: function() {
+  makePoem: function () {
     var seed = this.data.poemKeywords;
     var poemType = '4';
     var uuid = md5.hex_md5(seed + "-" + poemType);
@@ -74,25 +139,14 @@ Page({
 
   },
   //事件处理函数
-  bindPoemTap: function() {
+  bindPoemTap: function () {
     setTimeout(this.makePoem, 500);
   },
-  showNextPome : function(){
+  showNextPome: function () {
     var poemIndex = this.data.poemIndex;
     var poemLength = this.data.poems.length;
     poemIndex = (++poemIndex) % poemLength;
     console.log("poemIndex:" + poemIndex);
-    this.setData({ poemIndex: poemIndex});
-  },
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
+    this.setData({ poemIndex: poemIndex });
   }
 })
